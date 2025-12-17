d8e383e69576824d8097b5563ec5fbe86df49f0c
Ian Soares <ian.soares@aluno.uece.br>
2025-12-16 21:08:39 -0300

    Correção nova

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-numeros_teste = [ 12, 42, 83, 25, 67, 71, 3, 4, 94, 53, 100, 48, 19, 61, 86, 33, 13, 43, 84, 28, 81, 60, 6, 49, 40, 41, 38, 64, 44, 36, 45, 27, 11, 89, 63, 39, 
+numeros_de_teste = [ 12, 42, 83, 25, 67, 71, 3, 4, 94, 53, 100, 48, 19, 61, 86, 33, 13, 43, 84, 28, 81, 60, 6, 49, 40, 41, 38, 64, 44, 36, 45, 27, 11, 89, 63, 39, 
                  9, 58, 52, 17, 88, 77, 26, 62, 30, 96, 56, 65, 98, 99, 76, 73, 16, 95, 35, 87, 68, 69, 51, 92, 37, 75, 90, 82, 8, 18, 23, 93, 57, 10, 15, 97, 
                  14, 29, 7, 24, 31, 59, 78, 85, 5, 70, 55, 91, 47, 72, 2, 20, 34, 74, 50, 66, 32, 22, 54, 79, 21, 1, 80, 46]
 
@@ -150,7 +150,6 @@ def quick_sort(lista):
     quick_sort_aux(0, len(lista) - 1)
     return metricas["comparacoes"], metricas["trocas"]
 
-
 def heap_sort(lista):
     metricas = {"comparacoes": 0, "trocas": 0}
     tamanho = len(lista)
@@ -185,7 +184,6 @@ def heap_sort(lista):
 
     return metricas["comparacoes"], metricas["trocas"]
 
-
 def counting_sort(lista):
     comparacoes = len(lista)
     trocas = 0
@@ -211,7 +209,6 @@ def counting_sort(lista):
         trocas += 1
 
     return comparacoes, trocas
-
 
 algoritmos = {
     "Insertion Sort": insertion_sort,
