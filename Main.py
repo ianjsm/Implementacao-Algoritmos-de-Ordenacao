4970d562d6108698f1d530e75cafa337687e1200
Ian Soares <ian.soares@aluno.uece.br>
2025-12-16 21:06:59 -0300

    Correções

--- Main.py
+++ Main.py
@@ -1,11 +1,6 @@
-numeros_teste = [
-    12, 42, 83, 25, 67, 71, 3, 4, 94, 53, 100, 48, 19, 61, 86, 33, 13, 43, 84, 28,
-    81, 60, 6, 49, 40, 41, 38, 64, 44, 36, 45, 27, 11, 89, 63, 39, 9, 58, 52, 17,
-    88, 77, 26, 62, 30, 96, 56, 65, 98, 99, 76, 73, 16, 95, 35, 87, 68, 69, 51,
-    92, 37, 75, 90, 82, 8, 18, 23, 93, 57, 10, 15, 97, 14, 29, 7, 24, 31, 59,
-    78, 85, 5, 70, 55, 91, 47, 72, 2, 20, 34, 74, 50, 66, 32, 22, 54, 79, 21,
-    1, 80, 46
-]
+numeros_teste = [ 12, 42, 83, 25, 67, 71, 3, 4, 94, 53, 100, 48, 19, 61, 86, 33, 13, 43, 84, 28, 81, 60, 6, 49, 40, 41, 38, 64, 44, 36, 45, 27, 11, 89, 63, 39, 
+                 9, 58, 52, 17, 88, 77, 26, 62, 30, 96, 56, 65, 98, 99, 76, 73, 16, 95, 35, 87, 68, 69, 51, 92, 37, 75, 90, 82, 8, 18, 23, 93, 57, 10, 15, 97, 
+                 14, 29, 7, 24, 31, 59, 78, 85, 5, 70, 55, 91, 47, 72, 2, 20, 34, 74, 50, 66, 32, 22, 54, 79, 21, 1, 80, 46]
 
 def bubble_sort(lista):
     comparacoes = 0
@@ -20,7 +15,6 @@ def bubble_sort(lista):
                 trocas += 1
 
     return comparacoes, trocas 
-
 
 def selection_sort(lista):
     comparacoes = 0
@@ -39,7 +33,6 @@ def selection_sort(lista):
             trocas += 1
 
     return comparacoes, trocas
-
 
 def insertion_sort(lista):
     comparacoes = 0
@@ -62,7 +55,6 @@ def insertion_sort(lista):
         trocas += 1
 
     return comparacoes, trocas
-
 
 def shell_sort(lista):
     comparacoes = 0
@@ -90,7 +82,6 @@ def shell_sort(lista):
         intervalo //= 2
 
     return comparacoes, trocas
-
 
 def merge_sort(lista):
     metricas = {"comparacoes": 0, "trocas": 0}
@@ -131,7 +122,6 @@ def merge_sort(lista):
 
     merge_sort_aux(lista)
     return metricas["comparacoes"], metricas["trocas"]
-
 
 def quick_sort(lista):
     metricas = {"comparacoes": 0, "trocas": 0}
